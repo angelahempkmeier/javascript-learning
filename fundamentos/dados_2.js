e47da2815bebaad119353a0c5b613421b3bd9f85
aggsdp <aggsdpz@gmail.com>
2022-08-29 18:28:47 -0300

    trabalhando com dados - escolha de nomes e concatenacao

---
+++ fundamentos/dados_2.js
@@ -0,0 +1,9 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(preco*(1-desconto));
+
+let nome = 'Caderno';
+let categoria = 'Papelaria';
+console.log("Produto: " + nome 
++ ", Categoria: " + categoria);
