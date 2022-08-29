32362d90624d9146d427203ee104f1f58c7e8737
aggsdp <aggsdpz@gmail.com>
2022-08-29 18:05:51 -0300

    numeros pares e impares

---
+++ fundamentos/sentencaEBloco.js
@@ -0,0 +1,14 @@
+let i;
+
+console.log('Números ímpares:')
+for(i=1;i<=10;i++){
+    if(i%2!=0){
+        console.log(i);
+    }}
+console.log('Números pares:')
+for(i=1;i<=10;i++){
+    if(i%2==0) {
+        console.log(i);
+    }
+}
+    
