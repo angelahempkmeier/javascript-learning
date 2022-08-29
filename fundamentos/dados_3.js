83fab34a13daa07213ed1605cb7df8fba68f01b0
aggsdp <aggsdpz@gmail.com>
2022-08-29 18:34:30 -0300

    trabalhando com dados - tipos

---
+++ fundamentos/dados_3.js
@@ -0,0 +1,10 @@
+let idade = 31;
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = true;
+console.log(typeof estaChovendo); //boolean
+
+console.log(typeof "teste");
