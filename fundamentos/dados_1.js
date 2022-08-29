e41153e31c87bf8a17191814e6aeb5c9d335b692
aggsdp <aggsdpz@gmail.com>
2022-08-29 18:18:39 -0300

    trabalhando com dados - variáveis

---
+++ fundamentos/dados_1.js
@@ -0,0 +1,14 @@
+var nome = 'Caneta';
+var quantidade = 10; 
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = imposto + preco;
+
+console.log(nome);
+console.log(quantidade); //valor literal
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = 'canetica';
+console.log(nome);
