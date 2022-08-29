5dc4717768114ac9644d25f97d251fc0aa4095a9
aggsdp <aggsdpz@gmail.com>
2022-08-29 19:36:35 -0300

    area de uma circunferencia

---
+++ fundamentos/desafioArea.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+const RAIO = 10;
+let areaCirc;
+
+areaCirc = Math.pow(RAIO, 2)*PI;
+
+console.log("O valor da área de uma circunferência cujo raio é " + RAIO + ", é igual a " + areaCirc
+            + "m2");
+
+
+
+areaCirc = Math.PI*Math.pow(RAIO,2);
+console.log("O valor da área de uma circunferência cujo raio é " + RAIO + ", é igual a " + areaCirc
+            + "m2");
