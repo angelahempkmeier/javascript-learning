18522bf670effc314212e95c26fdc165b5c521d1
aggsdp <aggsdpz@gmail.com>
2022-08-30 08:40:09 -0300

    tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,7 @@
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.156;
+console.log(qualquer);
+console.log(typeof qualquer);
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,8 @@
+var a = 3;
+let b = 4;
+
+//var pode ser redeclarado, let não.
+
+const c = 5;
+
+//por padrão, sempre que uma var não mudar, atribuí-la como const
