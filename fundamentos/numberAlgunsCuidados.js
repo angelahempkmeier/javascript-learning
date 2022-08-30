fe30389de65e200185dba0111116e625120fb643
aggsdp <aggsdpz@gmail.com>
2022-08-30 09:21:41 -0300

    alguns cuidados com number

---
+++ fundamentos/numberAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7/0); //infinity
+console.log("10"/2); //5
+console.log("Show!"*2); //NaN
+console.log(0.1 + 0.7); //0.7999999999999
+//console.log(10.toString()); //Invalided expect token
+console.log((10).toString); //funciona
+console.log((10.356).toFixed(2)); //10.36
