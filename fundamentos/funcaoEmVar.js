26665929354bfccd476ddae3df7cf46efbfbf42d
Angela Silva <102918151+aggsdp@users.noreply.github.com>
2023-03-29 08:30:18 -0300

    Armazenar funcao em uma constante, arrow function, sintaxe simplificada

---
+++ fundamentos/funcaoEmVar.js
@@ -0,0 +1,12 @@
+const imprimirSoma = function (a, b) {
+    console.log(a+b);
+}
+
+//arrow
+
+const soma = (a, b) => {
+    return a + b;
+}
+
+//retorno implicito
+const subtracao = (a, b) => a - b;
