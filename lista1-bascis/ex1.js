cd5288a660622fd122a769c18c3526c86b7ce060
Angela Silva <aggsdpz@gmail.com>
2023-04-26 19:40:46 -0300

    inicio da lista de exercicio, ex1

---
+++ lista1-bascis/ex1.js
@@ -0,0 +1,9 @@
+function operations(n1, n2){
+    let soma = n1 + n2;
+    let sub = n1 - n2;
+    let mult = n1*n2;
+    let div = n1/n2;
+    console.log(`Soma: ${soma} \nSubtração: ${sub} \nMultiplicação: ${mult} \nDivisão: ${div}`);
+}
+
+const op1 = operations(1,1);
