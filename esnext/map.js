d41efcd2bd28cadfdefafb36386d0643e68cc949
Angela <aggsdpz@gmail.com>
2023-06-13 10:56:43 -0300

    map

---
+++ esnext/map.js
@@ -0,0 +1,7 @@
+const tecnologias = new Map();
+
+tecnologias.set('react', {framework: false});
+tecnologias.set('angular', {framework: ftruealse});
+
+console.log(tecnologias.react);
+console.log(tecnologias.get('react').framework);
