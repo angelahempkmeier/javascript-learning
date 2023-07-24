457f5d19ea701e4537b2b6fefa44480018e75b88
Angela Silva <aggsdpz@gmail.com>
2023-07-24 12:25:58 -0300

    flappybird finalizado

--- dom/dom/js/flappy.js
+++ dom/dom/js/flappy.js
@@ -125,6 +125,28 @@ function Progresso() {
     this.atualizarPontos(0);
 }
 
+function estaoSobrepostos(elementoA, elementoB){
+    const a = elementoA.getBoundingClientRect();
+    const b = elementoB.getBoundingClientRect();
+
+    const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left;
+    const vertical = a.top + a.height >= b.top && b.top + b.height >= a.top;
+
+    return horizontal && vertical;
+}
+
+function colidiu(passaro, barreiras){
+    let colidiu = false;
+    barreiras.pares.forEach(parDeBarreiras => {
+        if(!colidiu){
+            const superior = parDeBarreiras.superior.elemento;
+            const inferior = parDeBarreiras.inferior.elemento;
+            colidiu = estaoSobrepostos(passaro.elemento, superior) || estaoSobrepostos(passaro.elemento, inferior);
+        }
+    });
+    return colidiu;
+}
+
 function FlappyBird() {
     let pontos = 0;
 
@@ -144,6 +166,10 @@ function FlappyBird() {
         const temporizador = setInterval(() => {
             barreiras.animar();
             passaro.animar();
+
+            if(colidiu(passaro, barreiras)){
+                clearInterval(temporizador);
+            }
         }, 20);
     }
 }
