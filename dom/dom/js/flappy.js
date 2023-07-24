4c0d89e0a1c5c830100a3015f632fe85ef1e4363
Angela Silva <aggsdpz@gmail.com>
2023-07-24 11:42:27 -0300

    animacao

--- dom/dom/js/flappy.js
+++ dom/dom/js/flappy.js
@@ -1,10 +1,10 @@
-function novoElemento(tagName, className){
+function novoElemento(tagName, className) {
     const elem = document.createElement(tagName);
     elem.className = className;
     return elem;
 }
 
-function Barreira(reversa = false){
+function Barreira(reversa = false) {
     this.elemento = novoElemento('div', 'barreira');
 
     const borda = novoElemento('div', 'borda');
@@ -20,7 +20,7 @@ b.setAltura(200);
 document.querySelector('[wm-flappy').appendChild(b.elemento);
 */
 
-function ParDeBarreiras(altura, abertura, x){
+function ParDeBarreiras(altura, abertura, x) {
     this.elemento = novoElemento('div', 'par-de-barreiras');
 
     this.superior = new Barreira(true);
@@ -30,13 +30,13 @@ function ParDeBarreiras(altura, abertura, x){
     this.elemento.appendChild(this.inferior.elemento);
 
     this.sortearAbertura = () => {
-        const alturaSuperior = Math.random()*(altura - abertura);
+        const alturaSuperior = Math.random() * (altura - abertura);
         const alturaInferior = altura - abertura - alturaSuperior;
         this.superior.setAltura(alturaSuperior);
         this.inferior.setAltura(alturaInferior);
     }
 
-    this.getx = () => parseInt(this.elemento.style.left.split('px')[0]);
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0]);
     this.setX = x => this.elemento.style.left = `${x}px`;
     this.getLargura = () => this.elemento.clientWidth;
 
@@ -44,5 +44,42 @@ function ParDeBarreiras(altura, abertura, x){
     this.setX(x);
 }
 
+/*
 const b = new ParDeBarreiras(700, 200, 400);
 document.querySelector('[wm-flappy').appendChild(b.elemento);
+*/
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3)
+    ]
+
+    const deslocamento = 3;
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento);
+
+            if (par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length);
+                par.sortearAbertura();
+            }
+
+            const meio = largura/2;
+            const cruzouOMeio = par.getX() + deslocamento >= meio && par.getX() < meio;
+            if(cruzouOMeio){
+                notificarPonto();
+            } 
+        });
+    }
+}
+
+const barreiras = new Barreiras(700, 1200, 200, 400);
+const areaDoJogo = document.querySelector('[wm-flappy');
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento));
+
+setInterval(() => {
+    barreiras.animar();
+}, 20);
