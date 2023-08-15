660bd4296f86475181250ca36e75c4f90ca41986
Angela Silva <aggsdpz@gmail.com>
2023-08-15 17:08:53 -0300

    upload - nao funcionou direito

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -6,6 +6,29 @@ app.use(express.static('.'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback){
+        callback(null, './upload');
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+
+const upload = multer({storage}).single('arquivo');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err){
+            return res.end('Ocorreu um erro');
+        }
+
+        res.end('Concluído com sucesso.');
+    });
+});
+
 //app.get('/teste', (req, res) => res.send('ok'));
 
 app.listen(8080, () => console.log('Executando...'));
