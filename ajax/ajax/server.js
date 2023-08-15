18ca23273ace7863f0104e3d7332b3b6b8788cc2
Angela Silva <aggsdpz@gmail.com>
2023-08-15 16:47:47 -0300

    xmlhttprequest

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -6,6 +6,6 @@ app.use(express.static('.'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/teste', (req, res) => res.send('ok'));
+//app.get('/teste', (req, res) => res.send('ok'));
 
 app.listen(8080, () => console.log('Executando...'));
